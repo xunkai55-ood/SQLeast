dabd61b7218a0d697d7ce42297ee2aa9d3ca85b0
badpoet <badpoet55@gmail.com>
2015-01-09 04:46:24 +0800

    ql framework

---
+++ src/ql/parser.cpp
@@ -0,0 +1,17 @@
+#include "ql/query.h"
+#include "ql/parser.h"
+
+namespace sqleast {
+    namespace ql {
+
+        StructuredQuery* Parser::parse(string input) {
+            if (q_ != nullptr) delete q_;
+
+
+            // q_ = new CreateDBQuery();
+            // q_.type = Q_CREATE_DB;
+            // return q_;
+
+        }
+    }
+}
